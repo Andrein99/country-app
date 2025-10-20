4301a46c391ea3cec46948da851375bbcd27d30d
Andrés Correa <corrgom@gmail.com>
2025-10-20 16:50:35 -0500

    Se refactoriza un poco el código escrito en algunos de los componentes, para que sea más fácil emplearlos. Esto se realizó con unas funciones experimentales de Angular que trabajan como Resources. Se trabajó en la experiencia de usuario al mostrar diferentes estados al usuario cuando se trae información, si no se consigue o si carga. Se trabajó en el URL dinámico para amplicar la información de los países consultados.

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { map, Observable, catchError, throwError, delay } from 'rxjs';
 
 import type { RESTCountry } from '../interfaces/rest-countries.interfaces';
 import type { Country } from '../interfaces/country.interface';
@@ -21,7 +21,47 @@ export class CountryService {
       .pipe(
         map((restCountries) =>
           CountryMapper.mapRestCountryArrayToCountryArray(restCountries)
-        )
+        ),
+        catchError((error) => {
+          return throwError(
+            () => new Error(`No se pudo obtener países con el query: ${query}`)
+          );
+        })
+      );
+  }
+
+  searchByCountry(query: string): Observable<Country[]> {
+    query = query.toLowerCase();
+
+    return this.http.get<RESTCountry[]>(`${API_URL}/name/${query}`)
+      .pipe(
+        map((restCountries) =>
+          CountryMapper.mapRestCountryArrayToCountryArray(restCountries)
+        ),
+        // delay(3000), // Permite testear un tiempo para notar diferente información. Como la experiencia de usuario.
+        catchError((error) => {
+          return throwError(
+            () => new Error(`No se pudo obtener países con el query: ${query}`)
+          );
+        })
+      );
+  }
+
+  searchCountryByAlphaCode(code: string) {
+    const url = `${API_URL}/alpha/${code}`;
+
+    return this.http.get<RESTCountry[]>(url)
+      .pipe(
+        map((restCountries) =>
+          CountryMapper.mapRestCountryArrayToCountryArray(restCountries)
+        ),
+        map((countries) => countries.at(0)),
+        // delay(3000), // Permite testear un tiempo para notar diferente información. Como la experiencia de usuario.
+        catchError((error) => {
+          return throwError(
+            () => new Error(`No se pudo obtener países con el código: ${code}`)
+          );
+        })
       );
   }
 }
